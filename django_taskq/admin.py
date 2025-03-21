1814bb6b52f04ebcf5371a56b2f8ea800a7d833d
Aivars Kalvans <aivars.kalvans@gmail.com>
2025-03-21 20:50:45 +0200

    allow deletion

--- django_taskq/admin.py
+++ django_taskq/admin.py
@@ -29,9 +29,6 @@ class PendingTaskAdmin(admin.ModelAdmin):
     def has_add_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
 
 @admin.register(DirtyTask, FailedTask)
 class RestartableTaskAdmin(PendingTaskAdmin):
